846fe224fccb667018a687ffddfe9da61f956f4a
meryemozseven <ozsevenmeryem113@gmail.com>
2025-01-17 17:01:22 +0300

    değişiklik eklendi

---
+++ src/ödev/GenelTekrarAlti.java
@@ -0,0 +1,12 @@
+package ödev;
+
+public class GenelTekrarAlti {
+    public static void main(String[] args) {
+        /*SORU 16:
+            1 + 2 + 4 + 7 + 11 + 16 + 20 + 23  + 25 + 26 + 28 + 31 + 35 = 229 ?
+            işleminin sonucunu bulan java projesini yazınız.*/
+
+
+
+    }
+}
---
+++ src/ödev/GenelTekrarYedi.java
@@ -0,0 +1,7 @@
+package ödev;
+
+public class GenelTekrarYedi {
+    public static void main(String[] args) {
+
+    }
+}
