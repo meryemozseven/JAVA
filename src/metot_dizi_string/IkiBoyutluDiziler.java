8f2132401dce8a85456d6e9b0613211b6a27ffc8
meryemozseven <ozsevenmeryem113@gmail.com>
2025-03-14 00:18:04 +0300

    iki boyutlu diziler

---
+++ src/metot_dizi_string/IkiBoyutluDiziler.java
@@ -0,0 +1,42 @@
+package metot_dizi_string;
+
+public class IkiBoyutluDiziler {
+    public static void main(String[] args) {
+        //2X3'lük bir matris
+        int[][]sayilar=new int[2][3];
+        sayilar[0][0]=1;
+        sayilar[0][1]=2;
+        sayilar[0][2]=3;
+        sayilar[1][0]=4;
+        sayilar[1][1]=5;
+        sayilar[1][2]=6;
+//3*3 lük matris
+        int[][] sayilar2={{1,2,3},{4,5,6},{7,8,9}};;
+        for(int satir=0;satir<sayilar.length;satir++){
+            //sayilar[satir] belirli bir satırdaki elemanları temsil eden bir dizi
+            //olduğundan saayilar[satir].length o saırdaki sütun sayısını verir
+            for(int sutun=0;sutun<sayilar[satir].length;sutun++){
+                System.out.println(satir + ". satır " + sutun+ ". sutundaki değer " + sayilar[satir][sutun]);
+            }
+
+
+        }
+        System.out.println("++++++++++++++++++++++++++++++++");
+
+        int [][]borclar ={ {0,100,200} , {350,0,250} , {150,500,0} };
+        String[]kisiler ={"Emre","Hasan","Ali"} ;
+        for(int a=0 ; a<borclar.length ; a++ ){
+            for(int sutun2 = 0; sutun2<borclar[a].length ; sutun2++){
+                if(a=!sutun2){
+                    System.out.println(kisiler[a] + ". satır " + sutun2+ ". sutundaki değer " + borclar[a][sutun2]);
+
+                }
+
+            }
+        }
+
+
+
+
+    }
+}
